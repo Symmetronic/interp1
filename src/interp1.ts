d31215e8abcdd5c3c4cf6c927aa91c3e8b31cf0f
Simon Hochholzer <simon@symmetronic.com>
2020-03-23 16:15:23 +0100

    implement interpolation

--- src/interp1.ts
+++ src/interp1.ts
@@ -1,6 +1,10 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
+const zip = require('lodash/zip');
+
+/**
+ * Point data type.
+ */
+type Point = [number, number];
+
 /**
  * Interpolates values linearly in one dimension.
  * @param  xs  Array of sample points in reference space.
@@ -13,5 +17,62 @@ export default function interp1(
   ys: number[],
   xqs: number[],
 ): number[] {
-  return [];
+  /*
+   * Throws an error if number of sample points in reference space does not
+   * equal number of values in feature space
+   */
+  if (xs.length !== ys.length) {
+    throw new Error(
+      `Arrays of sample points xs and corresponding values ys have to
+      have equal length.`
+    );
+  }
+
+  /* Combine x and y arrays. */
+  const points: Point[] = zip(xs, ys) as Point[];
+
+  /* Sort points by x value in ascending order. */
+  points.sort((a, b) => {
+    const diff: number = a[0] - b[0];
+
+    /* Check if one x value occurs twice. */
+    if (diff === 0) {
+      throw new Error(`Two sample points x with same value ` + a[0] + `. This
+          is not allowed`);
+    }
+
+    return diff;
+  });
+
+  /* Interpolate values */
+  let yqs: number[] = xqs.map(xq => {
+    /* Determine previous and next points. */
+    // TODO: Put to helper function?
+    let prev: Point | undefined = undefined;
+    let next: Point | undefined = undefined;
+    for (let i: number = 0; i < points.length; i++) {
+      const point: Point = points[i];
+      if (point[0] <= xq) prev = point;
+      if (point[0] >= xq) next = point;
+      if (prev !== undefined && next !== undefined) break;
+    }
+
+    /* Check if value lies in interpolation range. */
+    if (prev === undefined || next === undefined) {
+      throw new Error(`Query value ` + xq + ` lies outside of range and
+          extrapolation is not supported.`);
+    }
+
+    /* Return exact value. */
+    if (prev === next) return prev[1];
+
+    /* Interpolate value. */
+    // TODO: Put to helper function.
+    const diff: number = next[0] - prev[0];
+    const lambda: number = (xq - prev[0]) / diff;
+    return (1 - lambda) * prev[1] + lambda * next[1];
+  });
+
+  /* Return result. */
+  return yqs;
 }
--- test/interp1.test.ts
+++ test/interp1.test.ts
@@ -1,10 +1,59 @@
 import interp1 from "../src/interp1"
 
-/**
- * Dummy test
- */
 describe('interp1', () => {
   it('interp1 exists', () => {
     expect(interp1).toBeTruthy();
   });
+
+  it('throws an error if the number of sample points does not equal the number of values', () => {
+    expect(() => {
+      interp1([1, 2, 3], [1, 2, 3, 4], [2, 2.5, 3]);
+    }).toThrowError();
+  });
+
+  it('throws an error if two values from reference space occur more than once', () => {
+    expect(() => {
+      interp1([5, 5, 8], [8, -3, 0], [6]);
+    }).toThrowError();
+  });
+
+  it('throws an error if query points lie outside of sample points range', () => {
+    expect(() => {
+      interp1([-5, 0, 7], [3, 9, -8], [-6]);
+    }).toThrowError();
+
+    expect(() => {
+      interp1([2, 4, 5], [-2, 4, -3], [7]);
+    }).toThrowError();
+  });
+
+  it('returns an empty array for empty query points', () => {
+    expect(interp1([], [], [])).toEqual([]);
+
+    expect(interp1([7, 8, 9], [-5, 9, 81], [])).toEqual([]);
+  });
+
+  it('returns exact values', () => {
+    expect(interp1(
+      [-8, 0, 5],
+      [1, 92, 4],
+      [-8, 5, 0]
+    )).toEqual([1, 4, 92]);
+  });
+
+  it('interpolates values linearly', () => {
+    expect(interp1(
+      [-7, -3, 0, 4, 9],
+      [0, 8, -4, -2, 3],
+      [-7, -5, -3, -1.5, 0, 2, 4, 6.5, 9]
+    )).toEqual([0, 4, 8, 2, -4, -3, -2, 0.5, 3]);
+  });
+
+  it('does not require the sample points to be in order', () => {
+    expect(interp1(
+      [9, -2, 0, 3],
+      [8, 0, 3, 1],
+      [-2, 0, 1.5, 3]
+    )).toEqual([0, 3, 2, 1]);
+  })
 });
