6779ec44481f6253b60a2e355bffb2fda8b568ed
Simon Hochholzer <simon@symmetronic.com>
2020-03-23 14:46:55 +0100

    initial commit

---
+++ src/interp1.ts
@@ -0,0 +1,17 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+  // import "core-js/fn/array.find"
+  // ...
+/**
+ * Interpolates values linearly in one dimension.
+ * @param  xs  Array of sample points in reference space.
+ * @param  ys  Array of values y(x) in feature space with length equal to xs.
+ * @param  xqs Array of query points in reference space. 
+ * @return     Interpolated values yq(xq) in feature space with length equal to xqs.
+ */
+export default function interp1(
+  xs: number[],
+  ys: number[],
+  xqs: number[],
+): number[] {
+  return [];
+}
---
+++ test/interp1.test.ts
@@ -0,0 +1,10 @@
+import interp1 from "../src/interp1"
+
+/**
+ * Dummy test
+ */
+describe('interp1', () => {
+  it('interp1 exists', () => {
+    expect(interp1).toBeTruthy();
+  });
+});
