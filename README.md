cea6fc9e9d53a71b7857a6e4005b01d7436d8816
Simon Hochholzer <simon@symmetronic.com>
2020-03-24 10:10:14 +0100

    enhance performance, add several interpol methods

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [MATLAB-inspired](https://www.mathworks.com/help/matlab/ref/interp1.html) 1-dimensional data interpolation.
 
-## Usage
+## Import
 
 You can import the function using.
 
@@ -10,13 +10,28 @@ You can import the function using.
 import interp1 from 'interp1'
 ```
 
-<!-- TODO: Add usage instructions -->
+## API
+
+```javascript
+vqs = interp1(xs, vs, xqs);
+vqs = interp1(xs, vs, xqs, method);
+```
+
+The function takes the following arguments:
+
+- `xs`: Array of independent sample points. No value may occur more than once.
+- `vs`: Array of dependent values v(x) with length equal to xs.
+- `xqs`: Array of query points.
+- `method`: Method of interpolation: `linear`, `nearest`, `next` or `previous`. Defaults to `linear`.
+
+It returns an array of interpolated values `vqs`, corresponding to the query values `xqs`.
 
 ## NPM scripts
 
- - `npm test`: Run test suite
- - `npm start`: Run `npm run build` in watch mode
- - `npm run test:watch`: Run test suite in [interactive watch mode](http://facebook.github.io/jest/docs/cli.html#watch)
- - `npm run test:prod`: Run linting and generate coverage
- - `npm run build`: Generate bundles and typings, create docs
- - `npm run lint`: Lints code
+- `npm install`: Install dependencies
+- `npm test`: Run test suite
+- `npm start`: Run `npm run build` in watch mode
+- `npm run test:watch`: Run test suite in [interactive watch mode](http://facebook.github.io/jest/docs/cli.html#watch)
+- `npm run test:prod`: Run linting and generate coverage
+- `npm run build`: Generate bundles and typings, create docs
+- `npm run lint`: Lints code
--- src/interp1.ts
+++ src/interp1.ts
@@ -1,76 +1,158 @@
+/*
+ * Dependencies.
+ */
+const unzip = require('lodash/unzip');
 const zip = require('lodash/zip');
 
 /**
- * Point data type.
+ * Method of interpolation.
  */
-type Point = [number, number];
+type InterpolationMethod = 'linear' | 'nearest' | 'next' | 'previous';
+
+/**
+ * Finds the index of range in which a query value is included in a sorted
+ * array with binary search.
+ * @param  xs Array sorted in ascending order.
+ * @param  xq Query value.
+ * @return    Index of range plus percentage to next index. 
+ */
+function binaryFindIndex(
+  xs: number[],
+  xq: number,
+): number {
+  /* Special case of only one element in array. */
+  if (xs.length === 1 && xs[0] === xq) return 0;
+
+  /* Determine bounds. */
+  let lower: number = 0;
+  let upper: number = xs.length - 1;
+
+  /* Find index of range. */
+  while (lower < upper) {
+    /* Determine test range. */
+    const mid: number = Math.floor((lower + upper) / 2);
+    const prev: number = xs[mid];
+    const next: number = xs[mid + 1];
+    if (xq < prev) {
+      /* Query value is below range. */
+      upper = mid;
+    } else if (xq > next) {
+      /* Query value is above range. */
+      lower = mid + 1;
+    } else {
+      /* Query value is in range. */
+      return mid + (xq - prev) / (next - prev); 
+    }
+  }
+
+  /* Range not found. */
+  return -1;
+}
+
+/**
+ * Interpolates a value.
+ * @param  vs     Array of values to interpolate between.
+ * @param  index  Index of new to be interpolated value.
+ * @param  method Kind of interpolation. Can be 'linear', 'nearest', 'next' or 'previous'.
+ * @return        Interpolated value.
+ */
+function interpolate(
+  vs: number[],
+  index: number,
+  method: InterpolationMethod = 'linear',
+): number {
+  /* Determine previous and next indices. */
+  const prev: number = Math.floor(index);
+  const next: number = Math.ceil(index);
+
+  /* Return exact value. */
+  if (prev === next) return vs[prev];
+
+  /* Interpolate value. */
+  switch (method) {
+    case 'nearest': {
+      /* Nearest neighbour interpolation. */
+      return (index - prev < 0.5) ? vs[prev] : vs[next];
+    }
+    case 'next': {
+      /* Next neighbour interpolation. */
+      return vs[next];
+    }
+    case 'previous': {
+      /* Previous neighbour interpolation. */
+      return vs[prev];
+    }
+    case 'linear':
+    default: {
+      /* Linear interpolation. */
+      const lambda: number = index - prev;
+      return (1 - lambda) * vs[prev] + lambda * vs[next];
+    }
+  }
+}
 
 /**
  * Interpolates values linearly in one dimension.
- * @param  xs  Array of sample points in reference space.
- * @param  ys  Array of values y(x) in feature space with length equal to xs.
- * @param  xqs Array of query points in reference space. 
- * @return     Interpolated values yq(xq) in feature space with length equal to xqs.
+ * @param  xs     Array of independent sample points.
+ * @param  vs     Array of dependent values v(x) with length equal to xs.
+ * @param  xqs    Array of query points.
+ * @param  method Method of interpolation.
+ * @return        Interpolated values vq(xq) with length equal to xqs.
  */
 export default function interp1(
   xs: number[],
-  ys: number[],
+  vs: number[],
   xqs: number[],
+  method: InterpolationMethod = 'linear',
 ): number[] {
   /*
-   * Throws an error if number of sample points in reference space does not
-   * equal number of values in feature space
+   * Throws an error if number of independent sample points is not equal to
+   * the number of dependent values.
    */
-  if (xs.length !== ys.length) {
+  if (xs.length !== vs.length) {
     throw new Error(
-      `Arrays of sample points xs and corresponding values ys have to
-      have equal length.`
+      `Arrays of sample points xs and corresponding values vs have to have
+      equal length.`
     );
   }
 
-  /* Combine x and y arrays. */
-  const points: Point[] = zip(xs, ys) as Point[];
+  /* Combine x and v arrays. */
+  const zipped: number[][] = zip(xs, vs);
 
-  /* Sort points by x value in ascending order. */
-  points.sort((a, b) => {
+  /* Sort points by independent variabel in ascending order. */
+  zipped.sort((a, b) => {
     const diff: number = a[0] - b[0];
 
-    /* Check if one x value occurs twice. */
+    /* Check if some x value occurs twice. */
     if (diff === 0) {
-      throw new Error(`Two sample points x with same value ` + a[0] + `. This
-          is not allowed`);
+      throw new Error(
+        'Two sample points have equal value ' + a[0] + '. This is not allowed.'
+      );
     }
 
     return diff;
   });
 
+  /* Extract sorted x and v arrays */
+  const unzipped: number[][] = unzip(zipped);
+  const sortedX: number[] = unzipped[0];
+  const sortedV: number[] = unzipped[1];
+
   /* Interpolate values */
-  let yqs: number[] = xqs.map(xq => {
-    /* Determine previous and next points. */
-    // TODO: Put to helper function?
-    let prev: Point | undefined = undefined;
-    let next: Point | undefined = undefined;
-    for (let i: number = 0; i < points.length; i++) {
-      const point: Point = points[i];
-      if (point[0] <= xq) prev = point;
-      if (point[0] >= xq) next = point;
-      if (prev !== undefined && next !== undefined) break;
-    }
+  const yqs: number[] = xqs.map(xq => {
+    /* Determine index of range of query value. */
+    let index: number = binaryFindIndex(sortedX, xq);
 
     /* Check if value lies in interpolation range. */
-    if (prev === undefined || next === undefined) {
-      throw new Error(`Query value ` + xq + ` lies outside of range and
-          extrapolation is not supported.`);
+    if (index === -1) {
+      throw new Error(
+        `Query value ` + xq + ` lies outside of range. Extrapolation is not
+        supported.`
+      );
     }
 
-    /* Return exact value. */
-    if (prev === next) return prev[1];
-
     /* Interpolate value. */
-    // TODO: Put to helper function.
-    const diff: number = next[0] - prev[0];
-    const lambda: number = (xq - prev[0]) / diff;
-    return (1 - lambda) * prev[1] + lambda * next[1];
+    return interpolate(sortedV, index, method);
   });
 
   /* Return result. */
